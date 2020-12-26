e83a9f69958ec1d0938efc58952bddcc1869fee0
Tushar <tushardesarda@outlook.com>
2020-12-26 16:12:47 +0530

    removed lombok code and added getter setter

--- src/main/java/com/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/ecommerce/entity/ProductCategory.java
@@ -8,11 +8,8 @@ import java.util.Set;
 
 @Entity
 @Table(name="product_category")
-// @Data -- known bug
-@Getter
-@Setter
-public class ProductCategory {
 
+public class ProductCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -23,7 +20,29 @@ public class ProductCategory {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 }
 
 
